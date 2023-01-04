86cec65d1ceea53c11a709ae144a009170b7e6ef
EddyM6 <eduard.hovyan@gmail.com>
2023-01-04 17:06:52 +0400

    Initial commit

---
+++ app/src/main/java/am/sit/sitam/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package am.sit.sitam.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFF8C62C0)
+val Purple500 = Color(0xFF5303C5)
+val Purple700 = Color(0xFF220466)
+val Teal200 = Color(0xFF00887B)
